7c187073d3f3e47d054fe56d659182748fddbb8d
eloi <coucousalut32b@gmail.com>
2024-02-12 17:42:58 +0100

    devoir

--- Calculator.py
+++ Calculator.py
@@ -10,8 +10,8 @@ def realDivi(a, b):
 def sub(a, b):
     return a - b
 
-def pow(a, b):
-    return a ** b
+def pow_2_b(b):
+    return 2 ** b
 
 if __name__ == "__main__":
     
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         print("1. Sum")
         print("2. Real Division")
         print("3. Subtract")
-        print("4. Power")
+        print("4. Power (2^b)")
         print("5. Exit")
 
         choice = input("Enter choice (1/2/3/4/5): ")
@@ -43,7 +43,7 @@ if __name__ == "__main__":
             result = sub(num1, num2)
             print(f"Result: {result}")
         elif choice == '4':
-            result = pow(num1, num2)
+            result = pow_2_b(num2)
             print(f"Result: {result}")
         else:
             print("Invalid input. Please enter a valid choice.")
